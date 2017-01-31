ecc8600b9d876c827155d1b0356fd55c2dd32c84
Rohan Nair <r@rohannair.ca>
2017-01-30 22:36:52 -0500

    FUCK SQLITE

--- knexfile.js
+++ knexfile.js
@@ -1,13 +1,13 @@
 module.exports = {
-  development: {
-    client: 'sqlite3',
-      connection: {
-        filename: "./mydb.sqlite"
-      },
-      debug: true
-  },
-  production: {
+  // development: {
+  //   client: 'sqlite3',
+  //     connection: {
+  //       filename: "./mydb.sqlite"
+  //     },
+  //     debug: true
+  // },
+  // production: {
     client: 'pg',
     connection: `${process.env.DATABASE_URL}?ssl=true`
-  }
+  // }
 };
